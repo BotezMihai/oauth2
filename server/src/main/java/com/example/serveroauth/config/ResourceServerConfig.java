82353b0cda440d3f7c1ab0400fdf1118ccdfbf9b
BotezMihai <mihai.botezz@gmail.com>
2019-07-31 11:07:34 +0300

    server

--- server/src/main/java/com/example/serveroauth/config/ResourceServerConfig.java
+++ server/src/main/java/com/example/serveroauth/config/ResourceServerConfig.java
@@ -1,6 +1,10 @@
 package com.example.serveroauth.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -9,9 +13,33 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @Configuration
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
-
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Bean
+    public AuthenticationManager customAuthenticationManager() throws Exception {
+        return authenticationManager();
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.requestMatchers()
+                .antMatchers("/login", "/oauth/authorize")
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.parentAuthenticationManager(authenticationManager).inMemoryAuthentication()
+                .withUser("admin")
+                .password("admin")
+                .roles("Admin");
+
+
 
     }
 }
